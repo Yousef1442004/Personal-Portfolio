06a3208a38910dd237cd59d4e0ee0399fc04d001
Yousef1442004 <yousifahmed1444@gmail.com>
2025-04-26 19:40:13 +0800

    added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { useState } from 'react'
+import Navbar from './Components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Routes >
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
